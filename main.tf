245ef00c172e64666b80a04ee6945ff6946d7b89
Ryan Royals <ryan.royals@arkahna.io>
2023-11-09 10:47:09 +1030

    stage 1

--- main.tf
+++ main.tf
@@ -2,3 +2,19 @@ resource "azurerm_resource_group" "dddAdelaide" {
   location = var.location
   name     = join("-", concat(local.naming.prefix, ["rg"], local.naming.suffix))
 }
+
+resource "azurerm_virtual_network" "spoke" {
+  name                = join("-", concat(local.naming.prefix, ["vnet"], local.naming.suffix))
+  location            = azurerm_resource_group.dddAdelaide.location
+  resource_group_name = azurerm_resource_group.dddAdelaide.name
+
+  address_space = ["192.168.1.0/24"]
+  subnet {
+    name           = "vm"
+    address_prefix = "192.168.1.0/27"
+  }
+  subnet {
+    name           = "app"
+    address_prefix = "192.168.1.32/27"
+  }
+}
